32a021691a4b83d171a6ea561c91a3dce5294cab
Michal Hornak <hornak.michal67@gmail.com>
2021-04-23 14:14:55 +0200

    add new campground

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
+
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', (req, res) => {
   res.render('home');
 })
@@ -29,9 +32,20 @@ app.get('/campgrounds',  async (req, res) => {
   res.render('campgrounds/index', {campgrounds});
 })
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+  // res.send(req.body.campground);
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`campgrounds/${campground._id}`); //redirect!!
+  // res.render('campgrounds/index', {campgrounds});
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
   const {id} = req.params;
-  console.log(id);
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground });
 })
