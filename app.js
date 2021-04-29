7b554c507d5257e7d2aac871aec5006d470a65bc
myselko <hornak.michal67@gmail.com>
2021-04-29 13:09:22 +0200

    adding layouts, boilerplate for same stuff

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate'); //engine
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -19,6 +20,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
