6d3976d171224b6a29c843beb630aa67d3f5a816
Michal Hornak <the.hornakmichal@gmail.com>
2021-05-02 10:02:44 +0200

    bootstrap styling

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     let random1000 = Math.floor(Math.random() * 1000);
     let randomPlaces = Math.floor(Math.random() * 21);
     let randomDescriptors = Math.floor(Math.random() * 18);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`, 
+      image: 'https://source.unsplash.com/collection/220381',
+      description: 'This is despription, blalalsdffsd, imedspfse, sdfsfsdfsdf.',
+      price,
       // title: `${descriptors[randomDescriptors]} ${places[randomPlaces]}`,
       
     })
